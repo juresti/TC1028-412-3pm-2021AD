e18d2ffd58405e8464b1e296e1cf735d63b56e9f
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2021-09-06 15:05:54 -0500

    Update T3Ej9.py

--- Monday August 30/T3Ej9.py
+++ Monday August 30/T3Ej9.py
@@ -5,7 +5,7 @@ def reloj():
         while(minutos < 60):
             segundos = 0
             while(segundos < 60):
-                print(f"{horas}:{minutos}:{segundos}")
+                print(f"{horas:02}:{minutos:02}:{segundos:02}")
                 segundos += 1
             minutos += 1
         horas += 1
